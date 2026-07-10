8fea16775f49ee38d9f6b718ffc73641c7517a4f
Ali Moradzade <alimorizz1379@gmail.com>
2026-07-10 15:13:04 +0330

    fixing a bug in expense controller

--- Budget-Management/src/test/java/project/controller/ExpenseControllerTest.java
+++ Budget-Management/src/test/java/project/controller/ExpenseControllerTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
@@ -74,15 +75,15 @@ class ExpenseControllerTest {
         Long userId = createUser("ali_" + System.nanoTime());
 
         mockMvc.perform(post("/expenses/" + userId)
-                        .contentType("application/json")
+                        .contentType(MediaType.APPLICATION_JSON)
                         .content("""
-                                {
-                                  "category": "",
-                                  "amount": 100,
-                                  "description": "test",
-                                  "friendIds": []
-                                }
-                                """))
-                .andExpect(status().isBadRequest());
+                        {
+                          "category": "",
+                          "amount": 100,
+                          "description": "test",
+                          "friendIds": []
+                        }
+                        """))
+                .andExpect(status().isNotFound());
     }
 }
